d0d3b5b4e8cb5f2210f6ff591611e30238fffb41
kkoli-9528 <118213144+kkoli-9528@users.noreply.github.com>
2025-06-21 08:38:46 +0530

    Structure Your UI Data Into a Clean JavaScript Object

--- src/components/HeaderInfo.jsx
+++ src/components/HeaderInfo.jsx
@@ -2,36 +2,59 @@ import styles from "./HeaderInfo.module.css";
 import { FaPlus } from "react-icons/fa6";
 import { RxHamburgerMenu } from "react-icons/rx";
 
+const headerInfo = {
+  company: {
+    name: "Burman Hospitality Pvt.Ltd.",
+    location: ["Delhi", "Central Delhi", "110002"],
+    createdAt: "1 Jul 2022",
+    type: "POSTPAID",
+  },
+  revenue: {
+    total: "10,00,000",
+    received: "7,00,000",
+    notDue: "1,00,000",
+  },
+  overdue: [
+    { label: "A", amount: "20,000", color: "orange" },
+    { label: "B", amount: "50,000", color: "lightcoral" },
+    { label: "C", amount: "50,000", color: "#c5242c" },
+    { label: "D", amount: "30,000", color: "red" },
+  ],
+  meta: {
+    category: "Others - References",
+    leadSource: "-",
+    lastUpdated: "07 Nov 2023",
+    outstanding: "10,00,000",
+    lastOrderDate: "10 Dec 2023 03:30 PM",
+  },
+};
+
 const HeaderInfo = () => {
+  const { company, revenue, overdue, meta } = headerInfo;
+
   return (
     <div className={styles.container}>
       <div className={styles.headerSection}>
         <div className={styles.firstHeaderSection}>
           <div className={styles.firstHeaderSectionHeaderText}>
-            Burman Hospitality Pvt.Ltd.
+            {company.name}
           </div>
           <div className={styles.firstHeaderSectionSubText}>
+            {company.location.map((item, i) => (
+              <p key={i} className={styles.firstHeaderSectionSubTextFont}>
+                <span className={styles.dot}>●</span>
+                {item}
+              </p>
+            ))}
+            <p className={styles.dot}>|</p>
             <p className={styles.firstHeaderSectionSubTextFont}>
-              <span className={styles.dot}>●</span>Delhi
+              Created on: {company.createdAt}
             </p>
-            <p className={styles.firstHeaderSectionSubTextFont}>
-              <span className={styles.dot}>●</span> Central Delhi
-            </p>
-            <p className={styles.firstHeaderSectionSubTextFont}>
-              <span className={styles.dot}>●</span>110002
-            </p>
-            <p className={styles.dot} style={{ paddingRight: "6px" }}>
-              |
-            </p>
-            <p className={styles.firstHeaderSectionSubTextFont}>
-              Created on: 1 Jul 2022
-            </p>
-            <p className={styles.dot} style={{ paddingRight: "6px" }}>
-              |
-            </p>
-            <p className={styles.postPaid}>POSTPAID</p>
+            <p className={styles.dot}>|</p>
+            <p className={styles.postPaid}>{company.type}</p>
           </div>
         </div>
+
         <div className={styles.innerDiv}>
           <RxHamburgerMenu style={{ color: "#0691d1", fontSize: "30px" }} />
           <button type="button" className={styles.btnPrimary}>
@@ -40,29 +63,33 @@ const HeaderInfo = () => {
           </button>
         </div>
       </div>
+
       <hr className={styles.horizontalLine} />
+
       <div className={styles.headerMiddleSection}>
         <div className={styles.headerMiddleSectionLeft}>
           <div className={styles.middleSectionDivLeft}>
-            <p style={{ fontWeight: "600" }}>10,00,000</p>
+            <p style={{ fontWeight: "600" }}>{revenue.total}</p>
             <p style={{ color: "lightgray", fontWeight: "500" }}>
               Total Revenue
             </p>
           </div>
           <div className={styles.middleSectionDivLeft}>
-            <p style={{ fontWeight: "600" }}>7,00,000</p>
+            <p style={{ fontWeight: "600" }}>{revenue.received}</p>
             <p style={{ color: "lightseagreen", fontWeight: "500" }}>
               Payment Received
             </p>
           </div>
           <div className={styles.middleSectionDivLeft}>
-            <p style={{ fontWeight: "600" }}>1,00,000</p>
+            <p style={{ fontWeight: "600" }}>{revenue.notDue}</p>
             <p style={{ color: "rgb(0, 119, 255)", fontWeight: "500" }}>
               Not Yet Due
             </p>
           </div>
         </div>
+
         <p className={styles.dot2}>|</p>
+
         <div className={styles.headerMiddleSectionRight}>
           <div className={styles.middleSectionDivRight}>
             <p style={{ color: "red", fontWeight: "500" }}>
@@ -71,50 +98,45 @@ const HeaderInfo = () => {
               Overdue
             </p>
           </div>
-          <div className={styles.middleSectionDivRight}>
-            <p style={{ fontWeight: "600" }}>20,000</p>
-            <p style={{ color: "orange", fontWeight: "bold" }}>A</p>
-          </div>
-          <div className={styles.middleSectionDivRight}>
-            <p style={{ fontWeight: "600" }}>50,000</p>
-            <p style={{ color: "lightcoral", fontWeight: "bold" }}>B</p>
-          </div>
-          <div className={styles.middleSectionDivRight}>
-            <p style={{ fontWeight: "600" }}>50,000</p>
-            <p style={{ color: "#c5242c", fontWeight: "bold" }}>C</p>
-          </div>
-          <div className={styles.middleSectionDivRight}>
-            <p style={{ fontWeight: "600" }}>30,000</p>
-            <p style={{ color: "red", fontWeight: "bold" }}>D</p>
-          </div>
+
+          {overdue.map((item, i) => (
+            <div key={i} className={styles.middleSectionDivRight}>
+              <p style={{ fontWeight: "600" }}>{item.amount}</p>
+              <p style={{ color: item.color, fontWeight: "bold" }}>
+                {item.label}
+              </p>
+            </div>
+          ))}
         </div>
       </div>
+
       <hr className={styles.horizontalLine} />
+
       <div className={styles.lastSectionDiv}>
         <div>
           <p style={{ color: "gray", fontWeight: "500" }}>
             Latest Category - Source
           </p>
-          <p style={{ fontWeight: "600" }}>Others - References</p>
+          <p style={{ fontWeight: "600" }}>{meta.category}</p>
         </div>
         <div>
           <p style={{ color: "gray", fontWeight: "500" }}>
             Latest Category - Lead Source
           </p>
-          <p>-</p>
+          <p>{meta.leadSource}</p>
         </div>
         <div>
           <p style={{ color: "gray", fontWeight: "500" }}>Last Updated</p>
-          <p style={{ fontWeight: "600" }}>07 Nov 2023</p>
+          <p style={{ fontWeight: "600" }}>{meta.lastUpdated}</p>
         </div>
         <div>
           <p style={{ color: "gray", fontWeight: "500" }}>Total Outstanding</p>
-          <p style={{ color: "red", fontWeight: "600" }}>10,00,000</p>
+          <p style={{ color: "red", fontWeight: "600" }}>{meta.outstanding}</p>
         </div>
         <div>
           <p style={{ color: "gray", fontWeight: "500" }}>Last Order Date</p>
           <p style={{ color: "red", fontWeight: "600" }}>
-            10 Dec 2023 03:30 PM
+            {meta.lastOrderDate}
           </p>
         </div>
       </div>
--- src/components/RightSection.jsx
+++ src/components/RightSection.jsx
@@ -1,92 +1,92 @@
 import styles from "./RightSection.module.css";
 
+const timelineData = [
+  {
+    date: "11 Oct 2023",
+    entries: [
+      {
+        label: "Proposal Sent",
+        time: "05:30 PM",
+        status: "Discussion Done",
+        note:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero possimus vitae obcaecati beatae a laudantium.",
+        followUp: "11 Oct 23 10:30 AM",
+        author: "Bhinkumar Prasad",
+      },
+      {
+        label: "Interested",
+        time: "05:30 PM",
+        status: "Discussion Done",
+        note:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero possimus vitae obcaecati beatae a laudantium.",
+        followUp: "11 Oct 23 10:30 AM",
+        author: "Bhinkumar Prasad",
+      },
+    ],
+  },
+  {
+    date: "7 Oct 2023",
+    entries: [
+      {
+        label: "Proposal Sent",
+        time: "05:30 PM",
+        status: "Discussion Done",
+        note:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero possimus vitae obcaecati beatae a laudantium.",
+        followUp: "11 Oct 23 10:30 AM",
+        author: "Bhinkumar Prasad",
+      },
+      {
+        label: "Interested",
+        time: "05:30 PM",
+        status: "Discussion Done",
+        note:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero possimus vitae obcaecati beatae a laudantium.",
+        followUp: "11 Oct 23 10:30 AM",
+        author: "Bhinkumar Prasad",
+      },
+    ],
+  },
+];
+
 const RightSection = () => {
   return (
     <div style={{ marginTop: "2%" }}>
-      <center style={{ fontWeight: "600", fontSize: "x-large" }}>
-        11 Oct 2023
-      </center>
-      <div className={styles.container}>
-        <div className={styles.innerDiv}>
-          <div style={{ minWidth: "100px", textAlign: "end" }}>
-            Proposal Sent <br /> 05:30 PM
-          </div>
-          <div className={styles.dotLineWrapper}>
-            <span className={styles.dot}></span>
-            <span className={styles.verticalLine}></span>
-          </div>
-          <div>
-            <div>Discussn Done</div>
-            <div>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
-              possimus vitae obcaecati beatae a laudantium{" "}
-            </div>
-            <div>Follow Up: 11 Oct 23 10:30 AM</div>
-            <div style={{ textAlign: "right" }}>- Bhinkumar Prasad</div>
-          </div>
-        </div>
-        <div className={styles.innerDiv2}>
-          <div style={{ minWidth: "100px", textAlign: "end" }}>
-            Interested <br />
-            05:30 PM
-          </div>
-          <div className={styles.dotLineWrapper}>
-            <span className={styles.dot}></span>
-            <span className={styles.verticalLine}></span>
-          </div>
-          <div>
-            <div>Discussn Done</div>
-            <div>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
-              possimus vitae obcaecati beatae a laudantium{" "}
-            </div>
-            <div>Follow Up: 11 Oct 23 10:30 AM</div>
-            <div style={{ textAlign: "right" }}>- Bhinkumar Prasad</div>
+      {timelineData.map((section, dateIdx) => (
+        <div key={dateIdx}>
+          <center style={{ fontWeight: "600", fontSize: "x-large" }}>
+            {section.date}
+          </center>
+
+          <div className={styles.container}>
+            {section.entries.map((entry, idx) => (
+              <div
+                key={idx}
+                className={idx % 2 === 0 ? styles.innerDiv : styles.innerDiv2}
+              >
+                {/* Left time and label */}
+                <div style={{ minWidth: "100px", textAlign: "end" }}>
+                  {entry.label} <br /> {entry.time}
+                </div>
+
+                {/* Dot and Line */}
+                <div className={styles.dotLineWrapper}>
+                  <span className={styles.dot}></span>
+                  <span className={styles.verticalLine}></span>
+                </div>
+
+                {/* Right content */}
+                <div>
+                  <div>{entry.status}</div>
+                  <div>{entry.note}</div>
+                  <div>Follow Up: {entry.followUp}</div>
+                  <div style={{ textAlign: "right" }}>- {entry.author}</div>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
-      </div>
-      <center style={{ fontWeight: "600", fontSize: "x-large" }}>
-        7 Oct 2023
-      </center>
-      <div className={styles.container}>
-        <div className={styles.innerDiv}>
-          <div style={{ minWidth: "100px", textAlign: "end" }}>
-            Proposal Sent <br /> 05:30 PM
-          </div>
-          <div className={styles.dotLineWrapper}>
-            <span className={styles.dot}></span>
-            <span className={styles.verticalLine}></span>
-          </div>
-          <div>
-            <div>Discussn Done</div>
-            <div>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
-              possimus vitae obcaecati beatae a laudantium{" "}
-            </div>
-            <div>Follow Up: 11 Oct 23 10:30 AM</div>
-            <div style={{ textAlign: "right" }}>- Bhinkumar Prasad</div>
-          </div>
-        </div>
-        <div className={styles.innerDiv2}>
-          <div style={{ minWidth: "100px", textAlign: "end" }}>
-            Interested <br />
-            05:30 PM
-          </div>
-          <div className={styles.dotLineWrapper}>
-            <span className={styles.dot}></span>
-            <span className={styles.verticalLine}></span>
-          </div>
-          <div>
-            <div>Discussn Done</div>
-            <div>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
-              possimus vitae obcaecati beatae a laudantium{" "}
-            </div>
-            <div>Follow Up: 11 Oct 23 10:30 AM</div>
-            <div style={{ textAlign: "right" }}>- Bhinkumar Prasad</div>
-          </div>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
